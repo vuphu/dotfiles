bebc4cf1e2e34a6dc2ab50c4e1aa529e8b7f673c
vuphu <vuphu.itus@gmail.com>
2024-11-17 22:27:53 +0700

    macos: update set-defaults script

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -3,3 +3,5 @@
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write -g KeyRepeat -int 1
 defaults write -g InitialKeyRepeat -int 10
+
+defaults write -g NSAutomaticCapitalizationEnabled -bool false
