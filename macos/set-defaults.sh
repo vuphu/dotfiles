a4f745d2a4b3697c02a5ff20cc405b13bcdddeb7
vuphu <vuphu.itus@gmail.com>
2026-04-17 21:30:26 +0700

    scripts: update bootstrap

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -5,3 +5,5 @@ defaults write -g KeyRepeat -int 1
 defaults write -g InitialKeyRepeat -int 10
 
 defaults write -g NSAutomaticCapitalizationEnabled -bool false
+
+defaults write -g com.apple.keyboard.fnState -bool true
